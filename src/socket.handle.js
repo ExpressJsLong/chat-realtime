d64bd1399515677a3850ae958cf788194b7028d0
Log log <phamvanlong098@gmail.com>
2022-09-15 22:57:17 +0700

    fix: typing group

--- src/socket.handle.js
+++ src/socket.handle.js
@@ -16,11 +16,34 @@ export function socketHandle(io) {
     socket.on(socketEvent.chat.typingStart, (payload) => {
       console.log(`typing start payload: ${JSON.stringify(payload)}`);
       io.to(payload.to).emit(socketEvent.chat.typingStart, payload);
+
+      // send to user
+      if (payload.target == "USER") {
+        io.to(payload.to).emit(socketEvent.chat.typingStart, payload);
+      } else {
+        getGroupMembers(payload.to).forEach((item) => {
+          console.log(item);
+          io.to(String(item.userId)).emit(
+            socketEvent.chat.typingStart,
+            payload
+          );
+        });
+      }
     });
 
     socket.on(socketEvent.chat.typingEnd, (payload) => {
       console.log(`typing end payload: ${JSON.stringify(payload)}`);
       io.to(payload.to).emit(socketEvent.chat.typingEnd, payload);
+
+      // send to user
+      if (payload.target == "USER") {
+        io.to(payload.to).emit(socketEvent.chat.typingEnd, payload);
+      } else {
+        getGroupMembers(payload.to).forEach((item) => {
+          console.log(item);
+          io.to(String(item.userId)).emit(socketEvent.chat.typingEnd, payload);
+        });
+      }
     });
 
     socket.on(socketEvent.chat.sendMessages, (payload) => {
